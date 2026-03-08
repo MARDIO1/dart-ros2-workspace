39644c4ee6c122a50b61e88fea834877ae62fdb1
mardio <mardio@noreply.gitcode.com>
2026-03-08 17:11:28 +0800

    应该可以用

--- src/dart_mcu/Core/Inc/motor.h
+++ src/dart_mcu/Core/Inc/motor.h
@@ -10,86 +10,74 @@
 #include "inttypes.h"
 #include "task.h"
 
-namespace motor
-{
+namespace motor {
 
-enum E_MotorState
-{
-    IDLE,
-    RUNNING,
-    DISCONNECTED,
-    UNDEFINED
-};
+enum E_MotorState { IDLE, RUNNING, DISCONNECTED, UNDEFINED };
 
-enum E_MotorType
-{
-    M2006,
-    M3508,
-    GM6020
-};
+enum E_MotorType { M2006, M3508, GM6020, DM4310 };
 
 void update_can_array(uint8_t *aData, uint8_t id, int16_t output);
+/*class motor_dm{
 
-class motor_rm
-{
-  public:
-    // Current
-    int16_t target_current_; // 0-10000
+}*/
+class motor_rm {
+public:
+  // Current
+  int16_t target_current_; // 0-10000
 
-    // Velocity
-    int16_t current_velocity_; // RPM
-    int16_t max_current_;      // 0-10000
+  // Velocity
+  int16_t current_velocity_; // RPM
+  int16_t max_current_;      // 0-10000
 
-    // Round & Position
-    int current_round_;
-    int current_angle_;
+  // Round & Position
+  int current_round_;
+  int current_angle_;
 
-    // Update Time and Disconnect check
-    TickType_t last_update_time_;
+  // Update Time and Disconnect check
+  TickType_t last_update_time_;
 
-    // Type
-    E_MotorType motor_type_;
+  // Type
+  E_MotorType motor_type_;
 
-    // Can Interface
-    uint8_t motor_id_;
+  // Can Interface
+  uint8_t motor_id_;
 
-    // Motor State
-    E_MotorState motor_state_ = DISCONNECTED;
+  // Motor State
+  E_MotorState motor_state_ = DISCONNECTED;
 
-  private:
-    int current_angle_last_;
-    // Motor Angle Reverse Flag
-    bool angle_reverse_;
-    bool first_decode_ = false;
+private:
+  int current_angle_last_;
+  // Motor Angle Reverse Flag
+  bool angle_reverse_;
+  bool first_decode_ = false;
 
-    // Motor State Flag
-    E_MotorState motor_state_next_;
+  // Motor State Flag
+  E_MotorState motor_state_next_;
 
-  public:
-    void create(int16_t max_current, E_MotorType motor_type, uint8_t motor_id,
-                bool angle_reserve = false)
-    {
-        max_current_ = max_current;
-        angle_reverse_ = angle_reserve;
-        target_current_ = 0;
-        current_velocity_ = 0;
-        current_round_ = 0;
-        last_update_time_ = xTaskGetTickCount();
-        motor_state_ = DISCONNECTED;
-        motor_type_ = motor_type;
-        motor_id_ = motor_id;
-    }
+public:
+  void create(int16_t max_current, E_MotorType motor_type, uint8_t motor_id,
+              bool angle_reserve = false) {
+    max_current_ = max_current;
+    angle_reverse_ = angle_reserve;
+    target_current_ = 0;
+    current_velocity_ = 0;
+    current_round_ = 0;
+    last_update_time_ = xTaskGetTickCount();
+    motor_state_ = DISCONNECTED;
+    motor_type_ = motor_type;
+    motor_id_ = motor_id;
+  }
 
-    // Interface
-    void decodeCanMsg(CAN_RxHeaderTypeDef *rxHeader, const uint8_t *rxData);
+  // Interface
+  void decodeCanMsg(CAN_RxHeaderTypeDef *rxHeader, const uint8_t *rxData);
 
-    bool setNextState(E_MotorState state);
+  bool setNextState(E_MotorState state);
 
-    void resetRound();
+  void resetRound();
 
-    bool setCurrent(int16_t target_current);
+  bool setCurrent(int16_t target_current);
 
-    int16_t updateCurrent();
+  int16_t updateCurrent();
 };
 
 extern motor_rm MotorTriggerLS; // 扳机丝杆电机
--- src/dart_mcu/Core/Inc/state_machine.h
+++ src/dart_mcu/Core/Inc/state_machine.h
@@ -12,8 +12,7 @@
 
 using namespace openfsm;
 
-namespace state_machine
-{
+namespace state_machine {
 [[noreturn]] void fsm_thread(void *parameters);
 
 // 状态机:
@@ -22,96 +21,65 @@ namespace state_machine
 // 1. 保护状态 遥控器右打上
 // 2. 调试模式 遥控器右打中
 // 3. 比赛模式 Action = Wait, Launch, Reload, Reset 遥控器右打下
-enum E_Dart_State
-{
-    Boot = 100,
-    Protect = 101,
-    Remote = 102,
-    Match = 103
+enum E_Dart_State { Boot = 100, Protect = 101, Remote = 102, Match = 103 };
+
+enum E_Match_Actions { Enter, Wait, Launch, Reload, Exit, Undefined };
+
+enum E_ResetActionReturnState { Operating, Finished, Failed };
+
+enum E_Gate_State { OPENED, CLOSED, OPERATING };
+
+enum E_Target_Type {
+  Default,          // 开局默认/未选定/前哨站
+  Stationary,       // 基地固定目标
+  RandomStationary, // 基地随机固定目标
+  RandomMoving      // 基地随机移动目标
 };
 
-enum E_Match_Actions
-{
-    Enter,
-    Wait,
-    Launch,
-    Reload,
-    Exit,
-    Undefined
+struct FSM {
+  OpenFSM openFSM_;
+
+  virtual void start() = 0;
+
+  void update();
 };
 
-enum E_ResetActionReturnState
-{
-    Operating,
-    Finished,
-    Failed
-};
+struct Dart_FSM : public FSM {
+  bool boot_success = false;
 
-enum E_Gate_State
-{
-    OPENED,
-    CLOSED,
-    OPERATING
-};
+  // ActionResetMotors
+  uint8_t ActionResetMotors_Load_0_Reset_State = 0;
+  uint8_t ActionResetMotors_Load_1_Reset_State = 0;
+  uint8_t ActionResetMotors_both_initialized = 0;
+  uint8_t ActionResetMotors_TriggerLS_Reset_State = 0;
+  uint8_t ActionMatch_Launch_State = 0;
+  TickType_t ActionGeneral_Timer0_;
+  TickType_t ActionGeneral_Timer1_;
+  TickType_t ActionGeneral_Timer2_;
+  TickType_t ActionGeneral_Timer3_;
+  // ActionRemote
+  uint8_t ActionRemote_MotorLoad_State = 0;
+  bool ActionRemoteandMatch_launch_complete_ = false;
+  // Public
+  // 0: 空闲 1: 将Load电机拉到装填位置 2: 降下升降机 3: 放开扳机舵机 4:
+  // 将Load电机拉到初始位置 5: 升起升降机
+  // 这里要改，改成新的
+  uint8_t ActionRemoteandReload_Reload_State = 0;
+  uint8_t ActionRemoteandReload_Slidedown_State = 0;
+  uint8_t ActionRemoteandReload_Reset_State = 0;
+  uint8_t ActionMatch_Wait_last_game_progress = 0;
+  bool ActionReload_Slidedown_Judge = false;
+  // 该值当且仅当发射信号确认，并且在比赛模式中的时候生效。为真则会跳过一次发射确认过程。
+  bool ActionMatch_Wait_Continuous_Fire = false;
+  bool launch_operating_ = false;
+  bool reset_operating_ = false;
 
-enum E_Target_Type
-{
-    Default,          // 开局默认/未选定/前哨站
-    Stationary,       // 基地固定目标
-    RandomStationary, // 基地随机固定目标
-    RandomMoving      // 基地随机移动目标
-};
-
-struct FSM
-{
-    OpenFSM openFSM_;
-
-    virtual void start() = 0;
-
-    void update();
-};
-
-struct Dart_FSM : public FSM
-{
-    bool boot_success = false;
-
-    // ActionResetMotors
-    uint8_t ActionResetMotors_Load_0_Reset_State = 0;
-    uint8_t ActionResetMotors_Load_1_Reset_State = 0;
-    uint8_t ActionResetMotors_both_initialized = 0;
-    uint8_t ActionResetMotors_TriggerLS_Reset_State = 0;
-    uint8_t ActionMatch_Launch_State = 0;
-    TickType_t ActionGeneral_Timer0_;
-    TickType_t ActionGeneral_Timer1_;
-    TickType_t ActionGeneral_Timer2_;
-    TickType_t ActionGeneral_Timer3_;
-    // ActionRemote
-    uint8_t ActionRemote_MotorLoad_State = 0;
-    bool ActionRemoteandMatch_launch_complete_ = false;
-    // Public
-    // 0: 空闲 1: 将Load电机拉到装填位置 2: 降下升降机 3: 放开扳机舵机 4:
-    // 将Load电机拉到初始位置 5: 升起升降机
-    uint8_t ActionRemoteandReload_Reload_State = 0;
-    uint8_t ActionRemoteandReload_Slidedown_State = 0;
-    uint8_t ActionRemoteandReload_Reset_State = 0;
-    uint8_t ActionMatch_Wait_last_game_progress = 0;
-    bool ActionReload_Slidedown_Judge = false;
-    // 该值当且仅当发射信号确认，并且在比赛模式中的时候生效。为真则会跳过一次发射确认过程。
-    bool ActionMatch_Wait_Continuous_Fire = false;
-    bool launch_operating_ = false;
-    bool reset_operating_ = false;
-
-  public:
-    void start();
+public:
+  void start();
 };
 
 extern Dart_FSM dart_fsm;
-enum class UpsideState
-{
-    MovingUp,
-    MovingDown,
-    Idle
-};
+enum class UpsideState { MovingUp, MovingDown, Idle };
 
 extern UpsideState upside_state;
 } // namespace state_machine
